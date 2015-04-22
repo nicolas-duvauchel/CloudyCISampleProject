7678e9ff29ae33e124433083574a109f1184a951
Nicolas Duvauchel <njlgad-github@corefiling.com>
2015-04-22 15:56:47 +0100

    Empty project and test one with one failing test

---
+++ test/SampleApp.UnitTests/TestSample.cs
@@ -0,0 +1,14 @@
+﻿namespace SampleApp.UnitTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestSample
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
